ee32a7db177738ed21117b431a2c37c69d3fd9a3
Raimonds Kazuss <rkazush@gmail.com>
2022-06-02 19:30:41 +0300

    added data

--- src/data/data.js
+++ src/data/data.js
@@ -10,30 +10,54 @@ export const posts = [
     id: 1,
     title: "some title one",
     bg: bg1,
+    author: "John Doe",
+    description:
+      "Pet me pet me don't pet me snuggles up to shoulders or knees and purrs you to sleep prance along on top of the garden fence, annoy the neighbor's dog and make it bark. Stuff and things make muffins eat fish on floor, so sit by the fire haha you hold me hooman i scratch destroy the blinds. Cat slap dog in face. ",
+    date: "02 06 2022",
   },
   {
     id: 2,
     title: "some title two",
     bg: bg2,
+    author: "John Doe",
+    description:
+      "Pet me pet me don't pet me snuggles up to shoulders or knees and purrs you to sleep prance along on top of the garden fence, annoy the neighbor's dog and make it bark. Stuff and things make muffins eat fish on floor, so sit by the fire haha you hold me hooman i scratch destroy the blinds. Cat slap dog in face. ",
+    date: "02 06 2022",
   },
   {
     id: 3,
     title: "some title three",
     bg: bg3,
+    author: "John Doe",
+    description:
+      "Pet me pet me don't pet me snuggles up to shoulders or knees and purrs you to sleep prance along on top of the garden fence, annoy the neighbor's dog and make it bark. Stuff and things make muffins eat fish on floor, so sit by the fire haha you hold me hooman i scratch destroy the blinds. Cat slap dog in face. ",
+    date: "02 06 2022",
   },
   {
     id: 4,
     title: "some title four",
     bg: bg4,
+    author: "John Doe",
+    description:
+      "Pet me pet me don't pet me snuggles up to shoulders or knees and purrs you to sleep prance along on top of the garden fence, annoy the neighbor's dog and make it bark. Stuff and things make muffins eat fish on floor, so sit by the fire haha you hold me hooman i scratch destroy the blinds. Cat slap dog in face. ",
+    date: "02 06 2022",
   },
   {
     id: 5,
     title: "some title five",
     bg: bg5,
+    author: "John Doe",
+    description:
+      "Pet me pet me don't pet me snuggles up to shoulders or knees and purrs you to sleep prance along on top of the garden fence, annoy the neighbor's dog and make it bark. Stuff and things make muffins eat fish on floor, so sit by the fire haha you hold me hooman i scratch destroy the blinds. Cat slap dog in face. ",
+    date: "02 06 2022",
   },
   {
     id: 6,
     title: "some title six",
     bg: bg6,
+    author: "John Doe",
+    description:
+      "Pet me pet me don't pet me snuggles up to shoulders or knees and purrs you to sleep prance along on top of the garden fence, annoy the neighbor's dog and make it bark. Stuff and things make muffins eat fish on floor, so sit by the fire haha you hold me hooman i scratch destroy the blinds. Cat slap dog in face. ",
+    date: "02 06 2022",
   },
 ];
